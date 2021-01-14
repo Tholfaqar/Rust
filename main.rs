967ace07794a25a5626e5fa813dbecfb3cc678df
Dolf <tmardan@me.berkeley.edu>
2021-01-14 15:15:10 -0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,79 +1,79 @@
 use std::collections::HashMap;
 
-mod loans;
+// this method will calculate cash flow for the user 
+// (banks that are customers of Plaid for example)
+// add all income per month
+// add all debts per month
+// subtract total income from total debt to find 
+// cash flow per month
+pub fn calc_cashflow(income: HashMap<String, f32>, debts: HashMap<String, f32>) -> f32{
+    
+    let mut income_total: f32 = 0.0;
+    let mut debt_total: f32 = 0.0;
+
+    for (_income_name, income_amount) in &income
+    {
+        income_total += income_amount;
+    }
+
+    for (_debt_name, debt_amount)in &debts
+    {
+        debt_total += debt_amount;
+    }
+    
+    let cash_flow: f32 = income_total - debt_total;
+    
+    return cash_flow;
+}
+// this method will calculate debt to income ratio for the user 
+// (banks that are customers of Plaid for example)
+// add all income per month
+// add all debts per month
+// divide debt by income to find 
+// DTR
+pub fn calc_debt_income_ratio (income: HashMap<String, f32>, debts: HashMap<String, f32>) -> f32{
+
+    let mut income_total: f32 = 0.0;
+    let mut debt_total: f32 = 0.0;
+
+    for (_income_name, income_amount) in &income
+    {
+        income_total += income_amount;
+    }
+
+    for (_debt_name, debt_amount)in &debts
+    {
+        debt_total += debt_amount;
+    }
+    
+    let dtr: f32 = debt_total/income_total;
+    
+    return dtr;
+}
 
 pub fn main(){
-    let mut assets: Vec<i32> =Vec::new(); 
-    let mut debts: Vec<i32> = Vec::new();
 
-    //let mut assets_hm:HashMap<String, i32> = HashMap::new();
-    //let mut debts_hm:HashMap<String, i32> = HashMap::new();
+    let mut income: HashMap<String, f32> = HashMap::new();
+    let mut debts: HashMap<String, f32> = HashMap::new();
 
-    let mut assets_hm: HashMap<&String, i32> = HashMap::new();
-    let mut debts_hm: HashMap<&String, i32> = HashMap::new();
+    income.insert(String::from("rental 1"), 2500.00);
+    income.insert(String::from("rental 2"), 2700.00);
+    income.insert(String::from("rental 3"), 3000.00);
+    income.insert(String::from("rental 4"), 3500.00);
 
-    assets.push(20);
-    assets.push(30);
-
-    debts.push(5);
-    debts.push(10);
-
-    loans::calc_cashflow(assets, debts);
-}
-
-pub fn add_debt(debts_hm: &mut HashMap<&String, i32>, debt_info: String, debt_amount: i32){
-    debts_hm.insert(debt_info, debt_amount);
-}
-
-pub fn add_income(assets_hm: &mut HashMap<&String, i32>, income_info: String, income_amount: i32){
-    assets_hm.insert(income_info, income_amount);
-}
-
-pub fn init_assets(mut assets_hm: HashMap<String, i32>){
-    let mut info = String::from("rental 1");
-    let mut amount: i32;
-    amount = 2500;
+    debts.insert(String::from("rental tax 1"), 500.00);
+    debts.insert(String::from("rental mortgage 1"), 1300.00);
+    debts.insert(String::from("rental tax 2"), 600.00);
+    debts.insert(String::from("rental mortgage 2"), 1400.00);
+    debts.insert(String::from("rental tax 3"), 700.00);
+    debts.insert(String::from("rental mortgage 3"), 1500.00);
+    debts.insert(String::from("rental tax 4"), 800.00);
+    debts.insert(String::from("rental mortgage 4"), 1600.00);
     
-    add_income(assets_hm, info, amount);
-    /*
-    asset_hm.insert(String::from(), 2500);
-    asset_hm.insert(String::from("rental 2"), 2700);
-    asset_hm.insert(String::from("devidand c1"), 500);
-    asset_hm.insert(String::from("devidand c2"), 300);
-    asset_hm.insert(String::from("salary 1"), 6000);
-    */
+    // cloing HashMaps here so it does not go out of scope when we call cash_flow..
+    let dtr: f32 = calc_debt_income_ratio(income.clone(), debts.clone());
+    println!("debt to income ratio: {}%", dtr * 100.0);
 
-}
-
-pub fn init_debt(mut debts_hm: HashMap<String, i32>){
-    /*
-    let mut info = String::from("property tax 1");
-    let mut amount: i32;
-    amount = 400;
-    add_debt(debts_hm, info, amount);
-
-    info = String::from("property tax 2");
-    amount = 500;
-    add_debt(&debts_hm, info, amount);
-    
-    info = String::from("mortgage 1");
-    amount = 1200;
-    add_debt(debts_hm, info, amount);
-
-    info = String::from("mortgage 2");
-    amount = 1300;
-    add_debt(debts_hm, info, amount);
-
-    info = String::from("home mortgage");
-    amount = 1500;
-    add_debt(debts_hm, info, amount);
-    */
-    
-    /*
-    debts_hm.insert(String::from("property tax 1"), 400);
-    debts_hm.insert(String::from("property tax 2"), 500);
-    debts_hm.insert(String::from("mortgage 1"), 1200);
-    debts_hm.insert(String::from("mortgage 2"), 1300);
-    debts_hm.insert(String::from("home mortgage"), 1500);
-    */
+    let cash_flow: f32 = calc_cashflow(income, debts);
+    println!("cash flow: ${}", cash_flow);
 }
